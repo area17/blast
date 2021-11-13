89209cc528105ad76c73ee0de28c56d9eaa480da
Robert Byrnes <59289145+RobertByrnes@users.noreply.github.com>
2021-11-13 20:10:48 +0000

    Update manager.js

--- .storybook/manager.js
+++ .storybook/manager.js
@@ -1,9 +1,23 @@
-
 import { addons } from '@storybook/addons';
+import { themes } from '@storybook/theming';
 import theme from './theme';
 
-if(process.env.STORYBOOK_THEME){
-  addons.setConfig({
-    theme,
-  });
+const configTheme = JSON.parse(process.env.STORYBOOK_THEME);
+
+let storybookTheme = () => {
+  if (configTheme === "dark") {
+    addons.setConfig({ theme: themes.dark });
+  } else {
+    addons.setConfig({ theme: themes.normal });
+  }
+}
+
+let customTheme = () => {
+  addons.setConfig({ theme });
+}
+
+if (configTheme === "custom") {
+  customTheme();
+} else {
+  storybookTheme();
 }
