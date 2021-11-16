1e02245d275bac572419f43ec27e57ad72a37888
Tim Brook <tim@area17.com>
2021-11-16 17:58:29 -0600

    Update full width documentation component to better support percentage heights

--- resources/views/components/ui-docs/full-width.blade.php
+++ resources/views/components/ui-docs/full-width.blade.php
@@ -1,8 +1,18 @@
 <div>
     @foreach ($items as $key => $item)
         <div class="{{ $loop->index > 0 ? 'blast-mt-4 blast-pt-4 blast-border-t' : '' }} blast-border-solid blast-border-gray-200">
-            <div class="blast-w-full blast-h-10 blast-bg-blue-500" style="{{ $property ?? 'width' }}: {{ $item }}">
-            </div>
+            @if (isset($property) && Str::contains($property, 'height') && Str::endsWith($item, '%'))
+                <div class="blast-bg-gray-200 blast-h-72">
+            @endif
+
+                <div
+                    class="blast-w-full blast-h-10 blast-bg-blue-500"
+                    style="{{ $property ?? 'width' }}: {{ $item }};">
+                </div>
+
+            @if (isset($property) && Str::contains($property, 'height') && Str::endsWith($item, '%'))
+                </div>
+            @endif
 
             <div class="
                 blast-inline-block
