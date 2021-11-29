08cbfc6de064c5d0cc2072fcd8590a7349d97dc7
Robert Byrnes <59289145+RobertByrnes@users.noreply.github.com>
2021-11-29 18:38:28 +0000

    Feature/add support for action event handlers (#29)
    * Add expanded controls to preview.js, improved theme control through config, begin custom theming
    
    * Add ability to include custom theme from blast.php
    
    * resolve merge conflict
    
    * resolve merge conflict
    
    * Add docs page to custom theme and ship with custom theme example
    
    * Add support for Action Event Handlers. Add to parameters the events array in GenerateStories.php and update README.md
    
    * remove parameters maing actions root to storybook blade directive.
    
    * add new line to preview.js

--- src/Commands/GenerateStories.php
+++ src/Commands/GenerateStories.php
@@ -312,7 +312,10 @@ class GenerateStories extends Command
                         'code' => $this->getCodeSnippet($item['path']),
                     ],
                 ],
-            ],
+                'actions' => [
+                    'handles' => $this->getEvents($item)
+                ],
+            ]
         ];
 
         // build options array
@@ -473,6 +476,15 @@ class GenerateStories extends Command
         $snippet = preg_replace('/@storybook\(\[(.*)\]\)/sU', '', $contents);
 
         return trim($snippet);
+    }
+
+    private function getEvents(array $item) : array
+    { 
+        if (Arr::has($item, 'options.actions.handles')) {
+            return $item['options']['actions']['handles'];
+        } else {
+           return [];
+        }
     }
 }
 
