9f6c9a25bc28a761b2504bbedbbf4d6f750a568e
Tim Brook <tim@area17.com>
2022-11-15 15:04:16 -0600

    Start working on Storybook 7 support

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,7 +1,6 @@
 module.exports = {
   stories: ['../stories/**/*.stories.mdx', '../stories/**/*.stories.@(json)'],
   addons: [
-    '@storybook/addon-postcss',
     '@storybook/addon-links',
     '@storybook/addon-essentials',
     '@storybook/addon-a11y',
@@ -9,7 +8,14 @@ module.exports = {
     'storybook-source-code-addon',
     '@etchteam/storybook-addon-status'
   ],
-  core: {
-    builder: 'webpack5'
+  features: {
+    storyStoreV7: false
+  },
+  framework: {
+    name: '@storybook/server-webpack5',
+    options: {
+      quiet: true,
+      port: 6006
+    }
   }
 };
